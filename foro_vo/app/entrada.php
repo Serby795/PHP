11fa2d4725d530a433f3d6549a75a1e201b3e26a
Marius Bogdan Serban <102753713+Serby795@users.noreply.github.com>
2022-10-25 09:22:24 +0200

    Update entrada.php

--- foro_vo/app/entrada.php
+++ foro_vo/app/entrada.php
@@ -1,8 +1,8 @@
 <form name='entrada' method="POST">
 <table>
 <tr>
-<td>Nombre:</td><td> <input type="text" name="nombre" 
-    value="<?=(isset($_REQUEST['nombre']))?$_REQUEST['nombre']:''?>"></td></tr>
+<td>Nombre:</td><td> <input type="text" name="user" 
+    value="<?=(isset($_REQUEST['user']))?$_REQUEST['user']:''?>"></td></tr>
 <tr>
 <td>Contraseña: </td><td><input type="password" name="password"
     value="<?=(isset($_REQUEST['password']))?$_REQUEST['password']:''?>"></td>
