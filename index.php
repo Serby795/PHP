58c888fd767f0a7a53460fdf1eff2607b13c99b3
Marius Bogdan Serban <102753713+Serby795@users.noreply.github.com>
2023-01-14 01:43:49 +0100

    5 de 10 hechos

--- index.php
+++ index.php
@@ -75,7 +75,6 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
         switch ($_GET['orden']) {
             case "Nuevo":
                 crudAlta();
-                //comentario de prueba
                 break;
             case "Borrar":
                 crudBorrar($_GET['id']);
